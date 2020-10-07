604df26ceef04bc3d92d8983a8695c5184b4e7b3
Maria <mariachernovolos@gmail.com>
2020-10-08 00:04:28 +0300

    rendering Transaction;

--- src/components/Layout/Product/DetailsTransactions/DetailsTransactions.js
+++ src/components/Layout/Product/DetailsTransactions/DetailsTransactions.js
@@ -1,67 +1,52 @@
 import React, {useEffect} from "react";
-import {Container, Row, Col, ListGroup, Card} from "react-bootstrap";
+import {Container, Row, Col, ListGroup, ListGroupItem, Badge} from "react-bootstrap";
 import {shallowEqual, useDispatch, useSelector} from "react-redux";
-import {getTransactionsIdItem} from "../../../../actions/transactions";
+import {getTransactionItem} from "../../../../actions/transactions";
 
 export const DetailsTransactions = (props) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
         const id = props.match.params.id;
-        dispatch(getTransactionsIdItem(id));
-    }, [dispatch, props.match.params.id]);
+        console.log("~~~~~~~~~~~~~~~~~BEFORE DISPATCH");
+        dispatch(getTransactionItem(id));
+    }, []);
 
-    const transactionsId = useSelector((state) => state.transactions.transactionsId, shallowEqual);
-    console.log("transactionId", transactionsId);
+    const transaction = useSelector((state) => state.transactions.transaction, shallowEqual);
 
+    console.log("transactionId before map", transaction);
 
-
-    // const incomingTransactions = (transaction) => {
-    //     transaction.toys.map(({id, name, description, price, totalCost, quantity}) => {
-    //         return (
-    //             <Col sm={4}>
-    //                 <ListGroup key={id}>
-    //                     <ListGroupItem>
-    //                         <span>incoming</span>
-    //                     </ListGroupItem>
-    //                     <ListGroupItem>
-    //                         <span>{name}</span>
-    //                     </ListGroupItem>
-    //                     <ListGroupItem>
-    //                         <span>description: {description}</span>
-    //                     </ListGroupItem>
-    //                     <ListGroupItem>
-    //                         <span>price: {price}</span>
-    //                     </ListGroupItem>
-    //                     <ListGroupItem>
-    //                         <span>totalCost: {totalCost}</span>
-    //                     </ListGroupItem>
-    //                     <ListGroupItem>
-    //                         <span>quantity: {quantity}</span>
-    //                     </ListGroupItem>
-    //                     {/*<ListGroupItem key={category.id}>*/}
-    //                     {/*    <span>category: {category.name}</span>*/}
-    //                     {/*</ListGroupItem>*/}
-    //                 </ListGroup>
-    //             </Col>
-    //         )
-    //     })
-    // };
-    // let {toys} = transactionsId;
-    //
-    // console.log("toys", toys)
     return (
         <Container>
             <Row>
-                <Col sm={6}>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Header>Type: {transactionsId.type}</Card.Header>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>user Id: {transactionsId.userId}</ListGroup.Item>
-                            <ListGroup.Item>{console.log(transactionsId.toys)}</ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
+                {
+                    transaction && transaction.toys.map((toy) => {
+                        return (
+                            <Col sm={6} key={toy.id}>
+                                <ListGroup>
+                                    <ListGroupItem>
+                                        <span> name: {toy.name}</span>
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        <span>description: {toy.description}</span>
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        <span>price: {toy.price}</span>
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        <span>totalCost: {toy.totalCost}</span>
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        <span>quantity: {toy.quantity}</span>
+                                    </ListGroupItem>
+                                    <ListGroupItem key={toy.category.id}>
+                                        <span>category: {toy.category.name}</span>
+                                    </ListGroupItem>
+                                </ListGroup>
+                            </Col>
+                        )
+                    })
+                }
             </Row>
         </Container>
     )
@@ -69,37 +54,6 @@ export const DetailsTransactions = (props) => {
 
 export default DetailsTransactions;
 
-// {
-//     transactionsId.map(({toys}) => {
-//         return (
-//             toys.map((toy) => {
-//                 console.log(toy);
-//                 return (
-//                     <Col sm={4}>
-//                         <ListGroup key={toy.id}>
-//                             <ListGroupItem>
-//                                 <span>{toy.name}</span>
-//                             </ListGroupItem>
-//                             <ListGroupItem>
-//                                 <span>description: {toy.description}</span>
-//                             </ListGroupItem>
-//                             <ListGroupItem>
-//                                 <span>price: {toy.price}</span>
-//                             </ListGroupItem>
-//                             <ListGroupItem>
-//                                 <span>totalCost: {toy.totalCost}</span>
-//                             </ListGroupItem>
-//                             <ListGroupItem>
-//                                 <span>quantity: {toy.quantity}</span>
-//                             </ListGroupItem>
-//                             {/*<ListGroupItem key={category.id}>*/}
-//                             {/*    <span>category: {category.name}</span>*/}
-//                             {/*</ListGroupItem>*/}
-//                         </ListGroup>
-//                     </Col>
-//                 )
-//             })
-//         )
-//     })
-// }
+
+
 
