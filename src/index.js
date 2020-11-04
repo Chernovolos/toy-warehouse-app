e2d494031797fb205219bc0f1e52c956d5020f68
Maria <mariachernovolos@gmail.com>
2020-11-04 18:51:01 +0200

    added and connected react-redux-toastr library;

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import './style/scss/index.scss'
+import "./style/scss/index.scss";
+import ReduxToastr from "react-redux-toastr";
 
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
           <App />
+          <div>
+              <ReduxToastr
+                  preventDuplicates
+                  getState={(state) => state.toastr} // This is the default
+                  transitionIn="fadeIn"
+                  transitionOut="fadeOut"
+                  progressBar
+                  closeOnToastrClick/>
+          </div>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,13 @@ import toysReducer from "./toysReducer";
 import loginReducer from "./loginReducer";
 import categoryReducer from "./categoryReducer";
 import transactionReducer from "./transactionReducer";
+import {reducer as toastrReducer} from "react-redux-toastr";
+
 
 export default combineReducers({
-    toys: toysReducer,
+    toysReducer,
     login: loginReducer,
     categoryReducer: categoryReducer,
     transactions: transactionReducer,
+    toastr: toastrReducer,
 });
